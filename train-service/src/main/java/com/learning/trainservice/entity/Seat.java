03e064fa30e649db88ccc6cb8bc2496ec680d0b5
Rajendra Kumar Mahapatra <rmahapatra1999@gmail.com>
2024-05-22 00:16:06 +0530

    seat entity made, and other entity removed.

--- train-service/src/main/java/com/learning/trainservice/entity/Seat.java
+++ train-service/src/main/java/com/learning/trainservice/entity/Seat.java
@@ -1,5 +1,6 @@
 package com.learning.trainservice.entity;
 
+import com.learning.trainservice.entity.enums.CoachType;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,8 +14,15 @@ public class Seat extends BaseModel {
     @Column(name = "seat_number")
     private Integer seatNumber;
 
+    @Column(name = "coach_number")
+    private String coachNumber;
+
     @ManyToOne
-    @JoinColumn (name = "coach_id")
-    private Coach coach;
+    @JoinColumn(name = "train_id")
+    private Train train;
+
+    @Column(name = "coach_type")
+    @Enumerated(value = EnumType.STRING)
+    private CoachType coachType;
 
 }
